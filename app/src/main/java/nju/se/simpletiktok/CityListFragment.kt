044085aff4728a91c2374a53b93dc10767828229
lluviasnow <280897191@qq.com>
2021-07-22 12:18:24 +0800

    reformed code and changed like button

--- app/src/main/java/nju/se/simpletiktok/CityListFragment.kt
+++ app/src/main/java/nju/se/simpletiktok/CityListFragment.kt
@@ -1,28 +1,19 @@
 package nju.se.simpletiktok
-
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.telecom.Connection
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.bumptech.glide.Glide
-import kotlin.collections.ArrayList
-
-
 
 class CityListFragment : Fragment(R.layout.activity_city_list), VideoClickListener {
     private var videos: List<VideoInfo>? = null
     private var VideoList: RecyclerView? = null
     private var adapter: VideoListAdapter? = null
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,36 +22,31 @@ class CityListFragment : Fragment(R.layout.activity_city_list), VideoClickListen
     ): View? {
         val root = inflater.inflate(R.layout.activity_city_list, container, false)
         VideoList = root.findViewById(R.id.rcy_video_list)
-        val staggeredGridLayoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        val staggeredGridLayoutManager =
+            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         this.VideoList?.setLayoutManager(staggeredGridLayoutManager)//模仿抖音使用错排两列
         VideoInfoProvider.request(this::init)
-
-        return root;
-
+        return root
     }
 
-    private fun init(){
-        videos =VideoInfoProvider.allVideoInfo
-        //videos= ArrayList()//TODO replace with real videolist from args
+    private fun init() {
+        videos = VideoInfoProvider.allVideoInfo
         adapter = VideoListAdapter(videos, this)
-        adapter!!.videos=videos
-        adapter!!.videoClickListener=this
-        if(this.VideoList!=null) this.VideoList!!.setAdapter(adapter)
+        adapter!!.videos = videos
+        adapter!!.videoClickListener = this
+        if (this.VideoList != null) this.VideoList!!.setAdapter(adapter)
     }
 
     override fun onVideoClick(clickedItemIndex: Int) {
-        val videos: ArrayList<VideoInfo?> = ArrayList<VideoInfo?>(videos)
         val intent = Intent(context, VideoWatchActivity::class.java)
-  //      intent.putExtra("videos", videos)
         intent.putExtra("index", clickedItemIndex)
         startActivity(intent)
     }
 
     private inner class VideoListAdapter(
-        videos: List<VideoInfo>?,
+        var videos: List<VideoInfo>?,
         cityListActivity: VideoClickListener
-    ) :RecyclerView.Adapter<VideoListAdapter.VideoItemViewHolder>() {
-        var videos: List<VideoInfo>? = videos
+    ) : RecyclerView.Adapter<VideoListAdapter.VideoItemViewHolder>() {
         var videoClickListener: VideoClickListener? = cityListActivity
         var viewHolderCount = 0
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoItemViewHolder {
@@ -68,10 +54,9 @@ class CityListFragment : Fragment(R.layout.activity_city_list), VideoClickListen
             val view = inflater.inflate(R.layout.video_list_item, parent, false)
             val videoItemViewHolder = VideoItemViewHolder(view)
             val video: VideoInfo = videos!![viewHolderCount]
-            //Picasso.get().load(Uri.parse(video.thumbnails)).into(videoItemViewHolder.imgVideo)
             Glide.with(view.context)
                 .load(video.avatarUri)
-                .centerCrop()//按理说在这里实现了填满，就不需要在xml里面处理了
+                .centerCrop()
                 .into(videoItemViewHolder.imgVideo)
             viewHolderCount++
             return videoItemViewHolder
@@ -90,7 +75,6 @@ class CityListFragment : Fragment(R.layout.activity_city_list), VideoClickListen
             override fun onClick(view: View) {
                 videoClickListener?.onVideoClick(adapterPosition)
             }
-
             init {
                 imgVideo = itemView.findViewById(R.id.img_video)
                 imgVideoPlay = itemView.findViewById(R.id.img_video_play)
@@ -99,9 +83,8 @@ class CityListFragment : Fragment(R.layout.activity_city_list), VideoClickListen
             }
         }
     }
-
-
 }
+
 interface VideoClickListener {
     fun onVideoClick(clickedItemIndex: Int)
 }
--- app/src/main/java/nju/se/simpletiktok/VideoWatchActivity.kt
+++ app/src/main/java/nju/se/simpletiktok/VideoWatchActivity.kt
@@ -1,44 +1,26 @@
 package nju.se.simpletiktok
 
 import android.os.Bundle
-import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
-import kotlin.properties.Delegates
 
 class VideoWatchActivity : AppCompatActivity() {
     private lateinit var viewPager: ViewPager2
-    private lateinit var nickname: String
-    private lateinit var description: String
-    private lateinit var videoUri: String
-    private lateinit var avatarUri: String
-    private var likeCount by Delegates.notNull<Int>()
     private var index = 0
     private var videos: List<VideoInfo>? = null
-    var currentVideo : VideoInfo?=null;
-
+    var currentVideo: VideoInfo? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_watch)
         VideoInfoProvider.request(this::initViewPager)
-//        if (intent.extras == null || intent.extras!!.getParcelableArrayList<Parcelable>("videos") == null) {
-//            finish()
-//            return
-//        }
-        //videos = intent.extras!!.getParcelableArrayList<Parcelable>("videos")
-//        this.nickname= currentVideo!!.nickname
-//        this.description=currentVideo!!.description
-//        this.avatarUri=currentVideo!!.avatarUri
-//        this.likeCount=currentVideo!!.likeCounts
-//        this.videoUri=currentVideo!!.videoUri
     }
-    private fun initViewPager() {
-        videos =VideoInfoProvider.allVideoInfo
 
+    private fun initViewPager() {
+        videos = VideoInfoProvider.allVideoInfo
         index = intent.extras!!.getInt("index")
         currentVideo = videos?.get(this.index) as VideoInfo
         viewPager = findViewById(R.id.pager)
@@ -47,16 +29,15 @@ class VideoWatchActivity : AppCompatActivity() {
     }
 
     private inner class VideoPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
-        // Use two fake fragments to implement endless scrolling
         override fun getItemCount(): Int = VideoInfoProvider.pageNum
         override fun createFragment(position: Int): Fragment {
-            val videoInfo = VideoInfoProvider.allVideoInfo[position]
             return VideoItemFragment.newInstance(
                 videoUri = currentVideo!!.videoUri,
                 avatarUri = currentVideo!!.avatarUri,
                 nickname = currentVideo!!.nickname,
                 description = currentVideo!!.description,
-                likeCount = currentVideo!!.likeCounts)
+                likeCount = currentVideo!!.likeCounts
+            )
         }
     }
 }
